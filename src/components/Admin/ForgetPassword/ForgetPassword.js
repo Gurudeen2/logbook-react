dcd383b96d9281c707275356fd1088ea5604069a
ControlPlus <akeemtolanifatai@gmail.com>
2023-03-14 11:20:50 +0100

    new commit

--- src/components/Admin/ForgetPassword/ForgetPassword.js
+++ src/components/Admin/ForgetPassword/ForgetPassword.js
@@ -5,6 +5,37 @@ import ForgetPasswordForm from "./ForgetPasswordForm";
 
 const ForgetPassword = () => {
   const methods = useForm();
+
+  const verifyMail = async (data) => {
+    const body = {
+      service_id: "service_bhj0dj9",
+      template_id: "template_4266rip",
+      user_id: "RWW4ceiL2At5kTAKE",
+      template_params: {
+        from_name: "James",
+        to_name: "akeemtolani2@gmail.com",
+        message: "content msg",
+        reply_to: "don't reply to this image",
+      },
+    };
+
+    emailjs.send("service_bhj0dj9", "template_4266rip", {
+      from_name: "Tolani MCAN",
+      to_name: "akeemtolani2@gmail.com",
+      message: "content msg",
+      reply_to: "don't reply to this image",
+    });
+    const sendEmail = await fetch(
+      "https://api.emailjs.com/api/v1.0/email/send",
+      {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Context-Type": "application/json",
+        },
+      }
+    );
+  };
   return (
     <FormProvider {...methods}>
       <Form>
