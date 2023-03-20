ee01e4f268ae728b9504556e05e3401a6f01a583
ControlPlus <akeemtolanifatai@gmail.com>
2023-03-20 09:50:04 +0100

    new commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // import imgMeal from "../../assets/images/meals.jpg";
 import classes from "./Header.module.css";
 
 const Header = (props) => {
   return (
-    <header
-      className={classes.header}
-    >
+    <header className={classes.header}>
       <h1>{props.title}</h1>
+      <div style={{ textAlign: "right" }}>
+        <Link> Register CM</Link>
+      </div>
       {/* <HeaderCartButton onShowCart={props.onShowCart} /> */}
     </header>
   );
